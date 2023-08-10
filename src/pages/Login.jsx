4e5e8df7acec86dcbfb34b7a227336e5e8d37855
Ngoc Anh cau hinh <anhcu2101@gmail.com>
2023-08-10 20:50:49 +0700

    completed page login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../styles/login.css'
+import { Container , Row , Col , Form , FormGroup , Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import loginImg from '../assets/images/login.png'
+import userIcon from '../assets/images/user.png'
 const Login = () => {
+
+  const [credentials, setCredentials] = useState({
+        email: undefined,
+        password:undefined
+});
+
+  const handleClick =  (e)=>{
+    e.preventDefault();
+  }
+const handleChange = (e)=>{
+    setCredentials(prev => ({...prev, [e.target.id]:e.target.value})) // return value thông qua sự kiên e.targrt.id
+}
   return (
-    <div>Login</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='8' className='m-auto'>
+              <div className="login__container d-flex justify-content-between">
+                <div className="login__img">
+                  <img src={loginImg} alt="" srcset="" />
+                </div>
+                <div className="login__form">
+                  <div className="user">
+                    <img src={userIcon} alt="" />
+                    <h2>Login</h2>
+                  </div>
+                  <Form className='form' onSubmit={handleClick}>
+                    <FormGroup>
+                      <input type="email" placeholder='Email' required 
+                        id='email'
+                        onChange={handleChange} />
+                    </FormGroup>
+                    <FormGroup>
+                      <input type="password" placeholder='Password' required 
+                        id='password'
+                        onChange={handleChange} />
+                    </FormGroup>
+                    <Button className='btn secondary__btn auth__btn' type='submit'>Login</Button>
+                    <p>Don't have an account? <Link to='/register'>Create</Link></p>
+                  </Form>
+              </div>
+              </div>
+             </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
